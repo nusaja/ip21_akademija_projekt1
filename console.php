4ad404f47c01ef4ab2032fd513c7059dd6ffbf8c
Nuša <nusa.jazbinsek@gmail.com>
2022-06-19 23:13:56 +0200

    List the names of the dog breeds

--- console.php
+++ console.php
@@ -1,5 +1,19 @@
 <?php
 
-echo "Hello world";
+echo "Hello world \n\n";
+
+$api_url = 'https://api.thedogapi.com/v1/breeds';
+
+// Read JSON file
+$json_data = file_get_contents($api_url);
+
+// Decode JSON data into PHP array
+$array = json_decode($json_data, true);
+
+
+for ($i = 0; $i < count($array); $i++) {
+    echo $array[$i]['name'] . "\n"; 
+}
+
 
 
