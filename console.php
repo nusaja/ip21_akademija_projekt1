6a067364bcb6d7afe04e569e0e43579d3d2fdf25
Nuša <nusa.jazbinsek@gmail.com>
2022-06-20 13:38:21 +0200

    Search dog breeds

--- console.php
+++ console.php
@@ -2,18 +2,38 @@
 
 echo "Hello world \n\n";
 
-$api_url = 'https://api.thedogapi.com/v1/breeds';
-
-// Read JSON file
-$json_data = file_get_contents($api_url);
-
-// Decode JSON data into PHP array
-$array = json_decode($json_data, true);
-
-
-for ($i = 0; $i < count($array); $i++) {
-    echo $array[$i]['name'] . "\n"; 
+if (empty($argv[1])) {
+    ListAllBreeds();
+} else {
+    SearchBreeds($argv);
 }
 
+//$search = $argv[1]; 
+
+function ListAllBreeds() {
+    
+    $api_url = 'https://api.thedogapi.com/v1/breeds';
+    $json_data = file_get_contents($api_url);
+    $array = json_decode($json_data, true);
+ 
+    for ($i = 0; $i < count($array); $i++) {
+        echo $array[$i]['name'] . "\n"; 
+    }
+    
+}
+
+function SearchBreeds($argv) {
+
+    global $argv; 
+    $api_url_search = 'https://api.thedogapi.com/v1/breeds/search?q=' . $argv[1];
+    $json_data = file_get_contents($api_url_search);
+    $array_search = json_decode($json_data, true);
+    
+    
+    for ($i = 0; $i < count($array_search); $i++) {
+        echo $array_search[$i]['name'] . "\n"; 
+    }
+
+}
 
 
