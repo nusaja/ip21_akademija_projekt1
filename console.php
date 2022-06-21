d382c9bb4b5c512e9de0d7e813ff2f5a781a4de5
Nuša <nusa.jazbinsek@gmail.com>
2022-06-21 15:20:05 +0200

    Add error handling

--- console.php
+++ console.php
@@ -10,8 +10,19 @@ if (empty($argv[1])) {
 function ListAllBreeds() {
     
     $api_url = 'https://api.thedogapi.com/v1/breeds';
-    $json_data = file_get_contents($api_url);
+    $json_data = @file_get_contents($api_url);
+
+    if ($json_data === FALSE) { 
+        echo "Error cought.\n";
+        die;
+    } 
+
     $array = json_decode($json_data, true);
+
+    if ($array === null) {
+        echo "Json cannot be decoded.\n";
+        die;
+    }
  
     for ($i = 0; $i < count($array); $i++) {
         echo $array[$i]['name'] . "\n"; 
@@ -25,7 +36,11 @@ function SearchBreeds($argv) {
     $api_url_search = 'https://api.thedogapi.com/v1/breeds/search?q=' . $argv[1];
     $json_data = file_get_contents($api_url_search);
     $array_search = json_decode($json_data, true);
-    
+
+    if (sizeof($array_search) === 0) {
+        echo "No such breed.\n";
+        die;
+    }
     
     for ($i = 0; $i < count($array_search); $i++) {
         echo $array_search[$i]['name'] . "\n"; 
