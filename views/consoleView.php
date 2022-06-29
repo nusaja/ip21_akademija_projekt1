a142cfe84d6732c7f6598791c6309d42b4579ee7
Nuša <nusa.jazbinsek@gmail.com>
2022-06-29 15:26:12 +0200

    Fix

--- views/consoleView.php
+++ views/consoleView.php
@@ -2,7 +2,7 @@
 
 class ConsoleView {
 
-    function showList(array $list) {
+    public function showList(array $list) {
     
         if (empty($list)) {
             echo "No results found.\n";
